078a940b01793e4b1c825c9b52c2b9df0bad2cbf
taik0 <suikogaiden@gmail.com>
2017-06-26 15:39:32 +0200

    Remove sample.txt and use a bigger text
    Update test with the new changes

--- huffman/test.py
+++ huffman/test.py
@@ -3,13 +3,12 @@
 from gen_freq_map import gen_freq_map
 from huffman import Huffman
 
-dataset = 'abcdefghijklmnopqrstuvwxyz '
-#dataset = 'abcdef'
-f = open('sample.txt', 'r')
-
-freq_map = gen_freq_map(dataset, f.read())
-
+f = open('dqdlm.txt', 'rb')
+freq_map = gen_freq_map(f.read())
 x = Huffman(freq_map)
+t = open('dqdlm.txt', 'rb')
 
-msg = x.encode("hola que tal")
-print x.decode(msg)
+msg = x.encode(t.read())
+
+print "Msg len: ", len(msg)
+print "Msg in MB: ", len(msg)/(8.0*1024**2)
